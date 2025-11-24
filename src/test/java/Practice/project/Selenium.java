f2918a01bc9ce1f828337391bdaf2a75a70dd12b
unknown <Raju@DESKTOP-LB3I86O.WIN>
2025-11-24 23:26:11 +0530

    Updated the timeouts code

--- src/test/java/Practice/project/Selenium.java
+++ src/test/java/Practice/project/Selenium.java
@@ -1,5 +1,7 @@
 package Practice.project;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -14,13 +16,13 @@ public class Selenium {
 		WebDriverManager.chromedriver().setup();
 		//ChromeOptions A = new ChromeOptions();
 		WebDriver driver = new ChromeDriver();
-		
+		driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(5));
 		driver.get("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-java/4.38.0");
 		driver.manage().window().fullscreen();
-		Thread.sleep(4000);
+		//Thread.sleep(4000);
 		String title = driver.getTitle();
 		System.out.println(title);
-		Thread.sleep(3000);
+		//Thread.sleep(3000);
 		driver.navigate().to("https://www.amazon.in/");
 		driver.manage().window().fullscreen();
 		driver.quit();
